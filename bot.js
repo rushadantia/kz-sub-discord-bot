e0eff0690fd2f12086a03e82ef239ee59a8d8edc
rushadantia <rushadantia@gmail.com>
2020-08-29 23:15:38 -0400

    works poggers

--- bot.js
+++ bot.js
@@ -5,11 +5,18 @@ const bot = new Discord.Client()
 bot.login(process.env.DISCORD_TOKEN)
 
 const TWITCH_SUB_ROLE = process.env.TWITCH_SUB_ROLE
+const WHITELIST_FILENAME = process.env.WHITELIST_FILENAME
+const DH_USER = process.env.DH_USER
+const DH_PASS = process.env.DH_PASS
+const DH_SERVER_ID = process.env.DH_SERVER_ID
 
 const k = require('keyv');
 
 const db_string = "postgresql://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@" + process.env.DB_HOST + ":" + process.env.DB_PORT + "/" + process.env.DB_DATABASE_NAME
 const db = new k(db_string)
+
+var fs = require('fs');
+const request = require('request-promise');
 
 db.on('error', err => console.log('Connection Error', err));
 
@@ -35,6 +42,7 @@ bot.on('message', async (message) => {
             } else {
                 await db.set(message.author.id, id)
             }
+
         } else if (content == "!generate") {
             let subs = message.guild.roles.get(TWITCH_SUB_ROLE).members.map(member => [member.id, member.username])
             let result = await db.opts.store.query("SELECT * FROM keyv;")
@@ -42,23 +50,57 @@ bot.on('message', async (message) => {
             let map = new Map()
             message.guild.roles.get(TWITCH_SUB_ROLE).members.map(member => map.set(member.id, member.user.username))
 
-		var whitelist = ""
+            var whitelist = ""
 
             result.forEach((v, k) => {
                 let discord_id = String(v["key"]).slice(5)
                 let steam_id = JSON.parse(String(v["value"]))["value"]
 
-                if(map.has(discord_id)){
-                	whitelist += steam_id + "  \/\/" + map.get(discord_id) + "\n"
+                if (map.has(discord_id)) {
+                    whitelist += steam_id + "  \/\/" + map.get(discord_id) + "\n"
                 }
             })
-	message.channel.send("```" + whitelist + "```")
+
+            //write string to file
+            fs.writeFile(WHITELIST_FILENAME, whitelist, (err) => {
+                console.log("done")
+
+                request({
+                        url: `https://dathost.net/api/0.1/game-servers/${DH_SERVER_ID}/files/addons/sourcemod/configs/${WHITELIST_FILENAME}`,
+                        method: 'POST',
+                        auth: {
+                            user: DH_USER,
+                            password: DH_PASS
+                        },
+                        formData: {
+                            file: fs.createReadStream(WHITELIST_FILENAME)
+                        }
+                    })
+                    .then(() => {
+                        console.log(`uploaded ${WHITELIST_FILENAME}`)
+
+                        request({
+                                url: `https://dathost.net/api/0.1/game-servers/${DH_SERVER_ID}/console`,
+                                method: 'POST',
+                                auth: {
+                                    user: DH_USER,
+                                    password: DH_PASS
+                                },
+                                formData: {
+                                    line: "sm_whitelist_reload; sm_say whitelist reloaded"
+                                }
+                            })
+                            .then(() => {
+                                message.channel.send("`Server has been refreshed with new players`")
+                            })
+                            .catch(console.error)
+                    })
+                    .catch(console.error)
+            })
         }
     }
 
 })
-
-
 
 function print(x) { console.log(x) }
 /* sudo apt update
